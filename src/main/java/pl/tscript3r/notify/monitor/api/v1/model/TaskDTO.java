dcbb69dc50cd567f870a61c718cb31a94fc4f916
tscript3r <alek199202@gmail.com>
2019-03-18 12:02:03 +0100

    test fix - tests requires all args constructor on taskDTO, which has been added

--- src/main/java/pl/tscript3r/notify/monitor/api/v1/model/TaskDTO.java
+++ src/main/java/pl/tscript3r/notify/monitor/api/v1/model/TaskDTO.java
@@ -1,6 +1,7 @@
 package pl.tscript3r.notify.monitor.api.v1.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -11,6 +12,7 @@ import java.util.Set;
 
 @Getter
 @Setter
+@AllArgsConstructor
 @NoArgsConstructor
 public class TaskDTO {
 
